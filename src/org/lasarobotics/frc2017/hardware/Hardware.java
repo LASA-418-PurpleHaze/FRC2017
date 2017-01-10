671c5e523e92ca3f8e6bcdc3d7ffb5f4e4841124
Jessica <jngitre@gmail.com>
2017-01-10 16:59:16 -0600

    started HazySubsystem and Hardware

--- src/org/lasarobotics/frc2017/hardware/Hardware.java
+++ src/org/lasarobotics/frc2017/hardware/Hardware.java
@@ -1,5 +1,14 @@
 package org.lasarobotics.frc2017.hardware;
 
-public class Hardware {
+public class Hardware implements Runnable {
     
+    private static Hardware instance;
+
+    public static Hardware getInstance() {
+        return (instance == null) ? instance = new Hardware() : instance;
+    }
+
+    @Override
+    public void run() {
+    }
 }
--- src/org/lasarobotics/frc2017/subsystem/HazySubsystem.java
+++ src/org/lasarobotics/frc2017/subsystem/HazySubsystem.java
@@ -1,5 +1,21 @@
 package org.lasarobotics.frc2017.subsystem;
 
-public class HazySubsystem {
+import org.lasarobotics.frc2017.hardware.Hardware;
+
+public abstract class HazySubsystem {
     
+    protected Hardware hardware;
+    
+    protected String errorMsg;
+    protected final String fileName = "logger.csv";
+    
+    protected HazySubsystem(){
+        hardware = Hardware.getInstance();
+    }
+    
+    public abstract void run();
+    
+    public abstract void initSubsystem();
+    
+    public abstract void pushToDashboard();
 }
