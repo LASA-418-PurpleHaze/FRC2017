f715469188e7e38ebff49902082b95a1864c10af
Jessica Gitre <jngitre@gmail.com>
2017-01-12 16:54:34 -0600

    drive motors

--- src/org/lasarobotics/frc2017/hardware/Hardware.java
+++ src/org/lasarobotics/frc2017/hardware/Hardware.java
@@ -1,11 +1,27 @@
 package org.lasarobotics.frc2017.hardware;
 
+import edu.wpi.first.wpilibj.VictorSP;
+import org.lasarobotics.frc2017.statics.Ports;
+
 public class Hardware implements Runnable {
-    
+
     private static Hardware instance;
+
+    private VictorSP leftDriveMotorA, leftDriveMotorB;
+    private VictorSP rightDriveMotorA, rightDriveMotorB;
 
     public static Hardware getInstance() {
         return (instance == null) ? instance = new Hardware() : instance;
+    }
+
+    public Hardware() {
+        leftDriveMotorA = new VictorSP(Ports.LEFT_DRIVE_MOTOR_A);
+        leftDriveMotorB = new VictorSP(Ports.LEFT_DRIVE_MOTOR_B);
+        rightDriveMotorA = new VictorSP(Ports.RIGHT_DRIVE_MOTOR_A);
+        rightDriveMotorB = new VictorSP(Ports.RIGHT_DRIVE_MOTOR_B);
+
+        rightDriveMotorA.setInverted(true);
+        rightDriveMotorB.setInverted(true);
     }
 
     @Override
--- src/org/lasarobotics/frc2017/statics/Ports.java
+++ src/org/lasarobotics/frc2017/statics/Ports.java
@@ -1,5 +1,10 @@
 package org.lasarobotics.frc2017.statics;
 
 public class Ports {
-    
+
+    public static final int LEFT_DRIVE_MOTOR_A = 0;
+    public static final int LEFT_DRIVE_MOTOR_B = 0;
+    public static final int RIGHT_DRIVE_MOTOR_A = 0;
+    public static final int RIGHT_DRIVE_MOTOR_B = 0;
+
 }
