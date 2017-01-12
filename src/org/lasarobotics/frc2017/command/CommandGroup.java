7c55abf2c718b8f6fa5273f8213fcf44235820a3
Jessica Gitre <jngitre@gmail.com>
2017-01-12 16:26:45 -0600

    copied over commandgroup

--- src/org/lasarobotics/frc2017/command/CommandGroup.java
+++ src/org/lasarobotics/frc2017/command/CommandGroup.java
@@ -1,5 +1,55 @@
 package org.lasarobotics.frc2017.command;
 
-public class CommandGroup {
-    
+import edu.wpi.first.wpilibj.Timer;
+import java.util.Iterator;
+import java.util.LinkedList;
+
+public class CommandGroup extends Command {
+
+    private LinkedList<Command> commands = new LinkedList<>();
+
+    public CommandGroup(String name, double timeOut) {
+        super(name, timeOut);
+    }
+
+    public void addCommand(Command c) {
+        commands.add(c);
+    }
+
+    @Override
+    public void start() {
+        startTime = Timer.getFPGATimestamp();
+        for (Command c : commands) {
+            c.startTime = Timer.getFPGATimestamp();
+            c.start();
+        }
+    }
+
+    @Override
+    public void run() {
+        Iterator it = commands.iterator();
+        while (it.hasNext()) {
+            Command temp = (Command) it.next();
+            if (temp.isDone()) {
+                temp.stop();
+                it.remove();
+            } else {
+                temp.run();
+            }
+        }
+    }
+
+    @Override
+    public boolean isDone() {
+        boolean done = true;
+        for (Command c : commands) {
+            done &= c.isDone();
+        }
+
+        return done;
+    }
+
+    @Override
+    public void stop() {
+    }
 }
