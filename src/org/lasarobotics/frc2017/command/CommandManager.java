cd1c6731eea9aa007b4d26d0e496f0116327a438
Jessica Gitre <jngitre@gmail.com>
2017-01-12 16:28:09 -0600

    copied over command manager

--- src/org/lasarobotics/frc2017/command/CommandManager.java
+++ src/org/lasarobotics/frc2017/command/CommandManager.java
@@ -1,5 +1,34 @@
 package org.lasarobotics.frc2017.command;
 
+import edu.wpi.first.wpilibj.Timer;
+import java.util.LinkedList;
+
 public class CommandManager {
-    
+
+    private static LinkedList<Command> list = new LinkedList<>();
+
+    public static void addCommand(Command c) {
+        list.add(c);
+    }
+
+    public static void run() {
+        if (!list.isEmpty()) {
+            Command command = list.getFirst();
+
+            if (!command.isStarted()) {
+                command.startTime = Timer.getFPGATimestamp();
+                command.start();
+                command.run();
+            } else if (command.isDone() || command.isTimedOut()) {
+                command.stop();
+                list.remove(command);
+            } else {
+                command.run();
+            }
+        }
+    }
+
+    public static void cancelAll() {
+        list.clear();
+    }
 }
