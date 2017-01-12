181d515987dd1a134c334f220f9d8dd2eb441d7c
Jessica Gitre <jngitre@gmail.com>
2017-01-12 16:24:38 -0600

    copied over command, set up drivetrain instances

--- src/org/lasarobotics/frc2017/command/Command.java
+++ src/org/lasarobotics/frc2017/command/Command.java
@@ -1,5 +1,51 @@
 package org.lasarobotics.frc2017.command;
 
-public class Command {
-    
+import edu.wpi.first.wpilibj.Timer;
+import org.lasarobotics.frc2017.hardware.Hardware;
+import org.lasarobotics.frc2017.subsystem.Drivetrain;
+
+public abstract class Command {
+
+    protected Drivetrain drivetrain;
+    protected Hardware hardware;
+
+    String name;
+
+    protected double startTime;
+    protected double currentTime;
+    double timeOut;
+
+    protected boolean isDone;
+    protected boolean stopped;
+    protected boolean shouldRun;
+    protected boolean isLeftDone;
+
+    public Command(String name, double timeOut) {
+        drivetrain = Drivetrain.getInstance();
+        hardware = Hardware.getInstance();
+
+        this.name = name;
+        this.timeOut = timeOut;
+        this.startTime = Double.MAX_VALUE;
+    }
+
+    public abstract void start();
+
+    public boolean isStarted() {
+        return startTime != Double.MAX_VALUE;
+    }
+
+    public abstract boolean isDone();
+
+    public abstract void run();
+
+    public abstract void stop();
+
+    public boolean isTimedOut() {
+        return (Timer.getFPGATimestamp() - startTime) > timeOut;
+    }
+
+    public void cancel() {
+        stopped = true;
+    }
 }
--- src/org/lasarobotics/frc2017/subsystem/Drivetrain.java
+++ src/org/lasarobotics/frc2017/subsystem/Drivetrain.java
@@ -2,6 +2,16 @@ package org.lasarobotics.frc2017.subsystem;
 
 public class Drivetrain extends HazySubsystem {
 
+    public Drivetrain() {
+
+    }
+    
+    private static Drivetrain instance;
+    
+    public static Drivetrain getInstance() {
+        return (instance == null) ? instance = new Drivetrain() : instance;
+    }
+
     @Override
     public void run() {
     }
@@ -13,5 +23,5 @@ public class Drivetrain extends HazySubsystem {
     @Override
     public void pushToDashboard() {
     }
-    
+
 }
