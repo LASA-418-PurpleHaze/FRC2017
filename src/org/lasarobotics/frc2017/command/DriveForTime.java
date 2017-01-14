8d96607dc97c194f9b7982ec5b0aa318bc5c035b
AaronG428 <aaron.m.garay@gmail.com>
2017-01-14 10:34:31 -0600

    added speed control for DriveForTime

--- src/org/lasarobotics/frc2017/command/DriveForTime.java
+++ src/org/lasarobotics/frc2017/command/DriveForTime.java
@@ -2,18 +2,19 @@ package org.lasarobotics.frc2017.command;
 
 public class DriveForTime extends Command {
 
-    private final double time;
+    private final double speed;
     
-    
-    public DriveForTime(String name, double to, double time) {
-        super(name, to);
-        this.time = time;
+    public DriveForTime(String name, double to) {
+        this(name, to, 1.0);
     }
-
+    
+    public DriveForTime(String name, double to, double speed){
+        super(name, to);
+        this.speed = speed;
+    }
     @Override
     public void start() {
-        hardware.setDriveSpeeds(1.0, 1.0);
-        
+        hardware.setDriveSpeeds(1.0, 1.0);   
     }
 
     @Override
