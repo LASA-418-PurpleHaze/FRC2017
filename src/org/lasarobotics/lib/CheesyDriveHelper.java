d9bf543dc1d987f2806734a6dee40336e0797523
Jessica Gitre <jngitre@gmail.com>
2017-01-16 10:08:08 -0600

    added cheesydrivehelper

---
+++ src/org/lasarobotics/lib/CheesyDriveHelper.java
@@ -0,0 +1,105 @@
+package org.lasarobotics.lib;
+
+import org.lasarobotics.frc2017.statics.Constants;
+
+public class CheesyDriveHelper {
+
+    double oldWheel, quickStopAccumulator;
+    private double leftPwm, rightPwm;
+
+    public void cheesyDrive(double throttle, double wheel, boolean isQuickTurn) {
+
+        double wheelNonLinearity;
+
+        double negInertia = wheel - oldWheel;
+        oldWheel = wheel;
+
+        wheelNonLinearity = 0.6;
+        // Apply a sin function that's scaled to make it feel better.
+        wheel = Math.sin(Math.PI / 2.0 * wheelNonLinearity * wheel)
+                / Math.sin(Math.PI / 2.0 * wheelNonLinearity);
+        wheel = Math.sin(Math.PI / 2.0 * wheelNonLinearity * wheel)
+                / Math.sin(Math.PI / 2.0 * wheelNonLinearity);
+        wheel = Math.sin(Math.PI / 2.0 * wheelNonLinearity * wheel)
+                / Math.sin(Math.PI / 2.0 * wheelNonLinearity);
+
+        double overPower;
+        double sensitivity;
+
+        double angularPower;
+        double linearPower;
+
+        // Negative inertia!
+        double negInertiaAccumulator = 0.0;
+        double negInertiaScalar;
+        negInertiaScalar = 4.0;
+        sensitivity = /*Constants.DRIVE_SENSITIVITY.getDouble(); */;
+
+        double negInertiaPower = negInertia * negInertiaScalar;
+        negInertiaAccumulator += negInertiaPower;
+
+        wheel = wheel + negInertiaAccumulator;
+        if (negInertiaAccumulator > 1) {
+            negInertiaAccumulator -= 1;
+        } else if (negInertiaAccumulator < -1) {
+            negInertiaAccumulator += 1;
+        } else {
+            negInertiaAccumulator = 0;
+        }
+        linearPower = throttle;
+
+        // Quickturn!
+        if (isQuickTurn) {
+            if (Math.abs(linearPower) < 0.2) {
+                double alpha = 0.1;
+                quickStopAccumulator = (1 - alpha) * quickStopAccumulator
+                        + alpha * limit(wheel, 1.0) * 5;
+            }
+            overPower = 1.0;
+            sensitivity = 1.0;
+            angularPower = wheel;
+        } else {
+            overPower = 0.0;
+            angularPower = Math.abs(throttle) * wheel * sensitivity
+                    - quickStopAccumulator;
+            if (quickStopAccumulator > 1) {
+                quickStopAccumulator -= 1;
+            } else if (quickStopAccumulator < -1) {
+                quickStopAccumulator += 1;
+            } else {
+                quickStopAccumulator = 0.0;
+            }
+        }
+
+        rightPwm = leftPwm = linearPower;
+        leftPwm += angularPower;
+        rightPwm -= angularPower;
+
+        if (leftPwm > 1.0) {
+            rightPwm -= overPower * (leftPwm - 1.0);
+            leftPwm = 1.0;
+        } else if (rightPwm > 1.0) {
+            leftPwm -= overPower * (rightPwm - 1.0);
+            rightPwm = 1.0;
+        } else if (leftPwm < -1.0) {
+            rightPwm += overPower * (-1.0 - leftPwm);
+            leftPwm = -1.0;
+        } else if (rightPwm < -1.0) {
+            leftPwm += overPower * (-1.0 - rightPwm);
+            rightPwm = -1.0;
+        }
+
+    }
+
+    private static double limit(double v, double limit) {
+        return (Math.abs(v) < limit) ? v : limit * (v < 0 ? -1 : 1);
+    }
+
+    public double getLeftPWM() {
+        return leftPwm;
+    }
+
+    public double getRightPWM() {
+        return rightPwm;
+    }
+}
