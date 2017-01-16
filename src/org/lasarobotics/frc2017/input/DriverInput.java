1d1079a6cc2441412675f6d62e0e7b57c66a334f
AaronG428 <aaron.m.garay@gmail.com>
2017-01-16 10:09:45 -0600

    started DriverInput

--- src/org/lasarobotics/frc2017/input/DriverInput.java
+++ src/org/lasarobotics/frc2017/input/DriverInput.java
@@ -1,11 +1,21 @@
 package org.lasarobotics.frc2017.input;
 
+import org.lasarobotics.lib.HazyJoystick;
+import org.lasarobotics.lib.
+import org.lasarobotics.frc2017.subsystem.Drivetrain;
+
 public class DriverInput implements Runnable{
     
     private static DriverInput instance;
+    private Drivetrain drivetrain;
     
+    private HazyJoystick driver = new HazyJoystick(0, 0.15);
+    private HazyJoystick operator = new HazyJoystick(1, 0.15);
+    
+    private CheesyDriveHelper cheesyDrive;
     private DriverInput(){
-        
+        drivetrain = Drivetrain.getInstance();
+        cheesyDrive = new CheesyDriveHelper();
     }
     
     public static DriverInput getInstance() {
@@ -14,5 +24,10 @@ public class DriverInput implements Runnable{
 
     @Override
     public void run() {
+        drivetrainControl();
+    }
+
+    private void drivetrainControl() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 }
