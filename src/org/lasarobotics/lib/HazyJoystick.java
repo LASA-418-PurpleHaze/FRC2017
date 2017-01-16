3deebc8bde77d037c849c02730c7f855f7c25676
Zayan Vohra <zayan.vohra@gmail.com>
2017-01-16 09:39:35 -0600

    Added content to HazyJoystick.java

--- src/org/lasarobotics/lib/HazyJoystick.java
+++ src/org/lasarobotics/lib/HazyJoystick.java
@@ -1,5 +1,89 @@
 package org.lasarobotics.lib;
 
+import edu.wpi.first.wpilibj.Joystick;
+        
+
 public class HazyJoystick {
+    
+    Joystick controller;
+    double deadband;
+    
+    public HazyJoystick(int joystickPort, double deadband){
+        
+        controller = new Joystick(joystickPort);
+        this.deadband = deadband;
+    }
+    
+    private double handleDeadband(double input){
+        
+        if (Math.abs(input) > deadband){
+            
+            if(input > 0){
+                return (input - deadband)/(1 - deadband);  
+            } else {
+                return (input + deadband)/(1 - deadband);
+            }
+            
+        } else {
+            return 0.0;
+        }
+    }
+    
+    public double getXAxis(){
+        return handleDeadband(controller.getRawAxis(0));
+    }
+    
+    public double getYAxis(){
+        return handleDeadband(controller.getRawAxis(1));
+    }
+    
+    public double getZAxis(){
+        return handleDeadband(controller.getRawAxis(2));
+    }
+    
+    public boolean getTrigger(){
+        return controller.getRawButton(1);
+    }
+    
+    public boolean getTopBackButton(){
+        return controller.getRawButton(2);
+    }
+    
+    public boolean getTopFrontButton(){
+        return controller.getRawButton(3);
+    }
+    
+    public boolean getTopleftButton(){
+        return controller.getRawButton(4);
+    }
+    
+    public boolean getTopRightButton(){
+        return controller.getRawButton(5);
+    }
+    
+    public boolean getLeftFrontButton(){
+        return controller.getRawButton(6);
+    }
+    
+    public boolean getLeftBackButton(){
+        return controller.getRawButton(7);
+    }
+    
+    public boolean getBackLeftButton(){
+        return controller.getRawButton(8);
+    }
+    
+    public boolean getBackRightButton(){
+        return controller.getRawButton(9);
+    }
+    
+    public boolean getRightBackButton(){
+        return controller.getRawButton(10);
+    }
+    
+    public boolean getRightFrontButton(){
+        return controller.getRawButton(11);
+    }
+    
     
 }
