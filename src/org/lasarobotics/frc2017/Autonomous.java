9d7ed32915803b2db97babd14f8f382ca5a608e2
AaronG428 <aaron.m.garay@gmail.com>
2017-01-19 17:33:00 -0600

    finished Robot.java

---
+++ src/org/lasarobotics/frc2017/Autonomous.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.lasarobotics.frc2017;
+
+/**
+ *
+ * @author airpendragon
+ */
+class Autonomous implements Runnable{
+
+    static Autonomous getInstance() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void run() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    void start() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
--- src/org/lasarobotics/frc2017/Robot.java
+++ src/org/lasarobotics/frc2017/Robot.java
@@ -3,29 +3,40 @@ package org.lasarobotics.frc2017;
 import edu.wpi.first.wpilibj.IterativeRobot;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import org.lasarobotics.frc2017.command.CommandManager;
+import org.lasarobotics.frc2017.hardware.Hardware;
 import org.lasarobotics.frc2017.input.DriverInput;
 import org.lasarobotics.frc2017.statics.Constants;
 import org.lasarobotics.frc2017.subsystem.Drivetrain;
-
+import org.lasarobotics.frc2017.subsystem.Intake;
+import org.lasarobotics.frc2017.subsystem.Shooter;
 public class Robot extends IterativeRobot {
     Drivetrain drivetrain;
     DriverInput driverInput;
-    //SensorInput sensorInput;
+    
     Constants constants;
+    Autonomous autonomous;
+    Hardware hardware;
     
     private static int time;
+    private Shooter shooter;
+    private Intake intake;
     
     private void pushToDashboard() {
         time++;
         SmartDashboard.putNumber("Time", time);
         drivetrain.pushToDashboard();
+        shooter.pushToDashboard();
+        intake.pushToDashboard();
+        
     }
     
     private void initSubsystems() {
-        //constants.loadFromFile();
-        //hazyVision.updateConstants();
+        Constants.load();
+        //If we use vision we would update constants here
         drivetrain.initSubsystem();
-        //sensorInput.start();
+        intake.initSubsystem();
+        shooter.initSubsystem();
+        hardware.start();
     }
     
     public static int getTime(){
@@ -34,41 +45,75 @@ public class Robot extends IterativeRobot {
     
     @Override
     public void robotInit(){
+        drivetrain = Drivetrain.getInstance();
+        shooter = Shooter.getInstance();
+        intake = Intake.getInstance();
+        
+        driverInput = DriverInput.getInstance();
+        hardware = Hardware.getInstance();
+        autonomous = Autonomous.getInstance();
+        autonomous.start();
+        
+        constants = new Constants();
         
     }
     
     @Override
     public void teleopInit(){
-        
+        CommandManager.cancelAll();
+        initSubsystems();
+        time=0;
     }
     
     @Override
     public void teleopPeriodic(){
-        
+        CommandManager.run();
+        driverInput.run();
+        drivetrain.run();
+        pushToDashboard();
     }
     
-    
+    public void teleopContinuous(){
+        hardware.run();
+        shooter.run();
+        intake.run();
+    }
     @Override
     public void autonomousInit(){
-        
+        initSubsystems();
+        CommandManager.cancelAll();
+        autonomous.run();
     }
     
     @Override
     public void autonomousPeriodic(){
+        pushToDashboard();
         
+    }
+    
+    public void autonomousContinuous(){
+        CommandManager.run();
+        hardware.run();
+        drivetrain.run();
+        shooter.run();
     }
     
     @Override
     public void disabledInit(){
         CommandManager.cancelAll();
         initSubsystems();
+        time=0;
     }
     
     @Override
     public void disabledPeriodic() {
-        //sensorInput.run();
         pushToDashboard();
     }
+    
+    public void disableContinuous(){
+        hardware.run();
+    }
+   
 
     
     
