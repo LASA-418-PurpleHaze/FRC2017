d897fbd4fa25fe8c228d6c6fbfe2408a0d2ba301
Jessica <jngitre@gmail.com>
2017-01-21 10:15:30 -0600

    farshot and closeshot

---
+++ src/org/lasarobotics/frc2017/command/CloseShot.java
@@ -0,0 +1,27 @@
+package org.lasarobotics.frc2017.command;
+
+public class CloseShot extends Command{
+
+    public CloseShot(String name, double timeOut) {
+        super(name, timeOut);
+    }
+
+    @Override
+    public void start() {
+    }
+
+    @Override
+    public boolean isDone() {
+        return true;
+    }
+
+    @Override
+    public void run() {
+        //pew pew
+    }
+
+    @Override
+    public void stop() {
+    }
+    
+}
---
+++ src/org/lasarobotics/frc2017/command/FarShot.java
@@ -0,0 +1,27 @@
+package org.lasarobotics.frc2017.command;
+
+public class FarShot extends Command{
+
+    public FarShot(String name, double timeOut) {
+        super(name, timeOut);
+    }
+
+    @Override
+    public void start() {
+    }
+
+    @Override
+    public boolean isDone() {
+        return true;
+    }
+
+    @Override
+    public void run() {
+        //peeeeeeeew peeeeeeeeeew
+    }
+
+    @Override
+    public void stop() {
+    }
+    
+}
