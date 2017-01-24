dbb119a8b02dc4c2ddeac9fec2b7165055de06db
Zayan Vohra <zayan.vohra@gmail.com>
2017-01-24 17:18:56 -0600

    Changed port numbers to reflect robot

--- src/org/lasarobotics/frc2017/statics/Ports.java
+++ src/org/lasarobotics/frc2017/statics/Ports.java
@@ -2,12 +2,12 @@ package org.lasarobotics.frc2017.statics;
 
 public class Ports {
 
-    public static final int LEFT_DRIVE_MOTOR_A = 0;
-    public static final int LEFT_DRIVE_MOTOR_B = 0;
+    public static final int LEFT_DRIVE_MOTOR_A = 3;
+    public static final int LEFT_DRIVE_MOTOR_B = 2;
     public static final int RIGHT_DRIVE_MOTOR_A = 0;
-    public static final int RIGHT_DRIVE_MOTOR_B = 0;
-    public static final int SHOOTER_MOTOR = 0;
-    public static final int INTAKE_MOTOR = 0;
-    public static final int SHOOTERSOLENOID = 0;
+    public static final int RIGHT_DRIVE_MOTOR_B = 1;
+    public static final int SHOOTER_MOTOR = -1;
+    public static final int INTAKE_MOTOR = -1;
+    public static final int SHOOTERSOLENOID = -1;
 
 }
