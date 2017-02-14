13b00b51b14c379ee86404a7c5a32da1764d0dcd
Jessica Gitre <jngitre@gmail.com>
2017-02-14 16:22:05 -0600

    ports for practice bot

--- src/org/lasarobotics/frc2017/statics/Ports.java
+++ src/org/lasarobotics/frc2017/statics/Ports.java
@@ -3,9 +3,9 @@ package org.lasarobotics.frc2017.statics;
 public class Ports {
 
     public static final int LEFT_DRIVE_MOTOR_A = 3;
-    public static final int LEFT_DRIVE_MOTOR_B = 2;
-    public static final int RIGHT_DRIVE_MOTOR_A = 0;
-    public static final int RIGHT_DRIVE_MOTOR_B = 1;
+    public static final int LEFT_DRIVE_MOTOR_B = 5;
+    public static final int RIGHT_DRIVE_MOTOR_A = 4;
+    public static final int RIGHT_DRIVE_MOTOR_B = 6;
 
     //Encoder ports
     public static final int DRIVE_ENCODER_L_A = 0;
