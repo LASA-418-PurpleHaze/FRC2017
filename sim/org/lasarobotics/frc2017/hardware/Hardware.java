3a9202c29994b1f7524fb8dcc0c504cbc9b95a8f
Gijs Landwehr <gijslandwehr@utexas.edu>
2017-05-24 01:50:11 -0500

    Attempted to make new freeform project with a very modified build script

---
+++ sim/org/lasarobotics/frc2017/hardware/Hardware.java
@@ -0,0 +1,5 @@
+package org.lasarobotics.frc2017.hardware;
+
+public class Hardware {
+    //This is the fake hardware class.
+}
---
+++ sim/org/lasarobotics/lib/HazyJoystick.java
@@ -0,0 +1,6 @@
+package org.lasarobotics.lib;
+
+public class HazyJoystick {
+    //This is the fake joystick class. This will replicate the normal
+    //one but instead use a windows joystick library.
+}
