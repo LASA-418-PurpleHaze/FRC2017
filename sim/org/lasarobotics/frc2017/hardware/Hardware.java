349135df4a35124d9b73c71a2db0b144dcdc5a1a
Zayan Vohra <zayan.vohra@gmail.com>
2017-06-06 21:56:50 -0500

    Added all methods in original Hardware.java

--- sim/org/lasarobotics/frc2017/hardware/Hardware.java
+++ sim/org/lasarobotics/frc2017/hardware/Hardware.java
@@ -2,4 +2,117 @@ package org.lasarobotics.frc2017.hardware;
 
 public class Hardware {
     //This is the fake hardware class.
+    //Comments are Zayan's hypothesis of what various methods will do, probably wrong
+    
+    public static Hardware instance;
+    
+    public Hardware(){
+        //Initialize all the fake hardware counters
+        
+        //Fake Motors
+        
+        //Fake Sensors
+    }
+    
+    public void run(){
+        //simulate running the robot and assign values to all the numbers
+    }
+    
+    public void reset(){
+        //resets all sim values back to 0
+    }
+    
+    public static Hardware getInstance(){
+        //Will return an instance of the hardware sim
+        return instance;
+    }
+    
+    private IntakeMode intakeMode;
+
+    public enum IntakeMode {
+        intaking, shooting;
+    }
+    
+    public void setIntakeMode(IntakeMode intakeMode){
+        //sets intake mode of the sim
+    }
+    
+    public void setIntakeOutput(double Output){
+        //sets output value for the intake
+    }
+    
+    public void setIntakeSolenoid(boolean down){
+        //whether sim input arm is down or not
+    }
+    
+    public void actuateIntake(boolean down){
+        //Does the same thing as the last function, in here bc I dont wanna break stuff
+    }
+    
+    public void setConveyorRollerSpeed(double speed){
+        //sets sim conveyor to speed
+    }
+    
+    public void setClimberSpeed(double speed){
+        //sets sim climber speed
+    }
+    
+    public void setShooterRPM(double speed){
+        //sets speeds for sim shooter motors(controlled by talon PID)
+    }
+    
+    public boolean isShooterRPMDone(){
+        //returns if fake shooter is close enough to the RPM we want
+        return true;
+    }
+    
+    public void setDriveSpeeds(double left, double right){
+        //uses left and right to set sim drive speeds
+    }
+    
+    public double getLeftDriveDistance(){
+        return 0;
+        //take sim drive distance and give it back
+        //originally took encoder value and multiplied by gear ratio
+    }
+    
+    public double getRightDriveDistance(){
+        return 0;
+    }
+    
+    public double getLeftDriveVelocity(){
+        return 0;
+    }
+    
+    public double getRightDriveVelocity(){
+        return 0;
+    }
+    
+    public double getNavXAngle(){
+        //returns angle the sim robot is at
+        return 0.0;
+    }
+    
+    public double getRobotAngle(){
+        return 0.0;
+        //returns a value called "robotangle", which is apparently different from the navx angle
+    }
+    
+    public void actuateGear(boolean out){
+        //actuates the sim gear pneumatic
+    }
+    
+    public double getClimberCurrent(){
+        return 0.0;
+        //returns the current running through climber to see it's power output
+    }
+    
+    public static double getCurrentTime(){
+        return 0.0;
+        //dunno how this'll work in a sim, maybe current computer time? Probably.
+    }
+    
+    public void pushToDashboard(){
+        //pushes stuff to the dashboard, which idk if we'll implement into the sim
+    }
 }
